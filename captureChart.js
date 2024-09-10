4610de59b1c221e6b8a4f63172eb1c1ed882450e
Shawn1042 <shawnholmes9921@gmail.com>
2024-09-10 16:40:37 -0400

    Update Puppeteer settings for Heroku

--- captureChart.js
+++ captureChart.js
@@ -21,7 +21,6 @@ export async function captureChart(playerName) {
       return null;
     }
 
-    // Adjust the viewport size to make the chart bigger
     const chartDimensions = await page.evaluate(() => {
       const canvas = document.querySelector('canvas#playerChart');
       if (canvas) {
@@ -31,7 +30,6 @@ export async function captureChart(playerName) {
       return { width: 1600, height: 1000 }; // Default dimensions if canvas is not found
     });
 
-    // Set a larger device scale factor to make the chart appear bigger
     await page.setViewport({ width: chartDimensions.width, height: chartDimensions.height, deviceScaleFactor: 2.5 });
 
     const screenshotPath = `./${playerName.replace(/ /g, '_')}_chart.png`;
